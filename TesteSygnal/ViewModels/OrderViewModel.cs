a5b0d91864fb123ebf0ea7098dccc3fd1e2ccdc5
IronProg <rnberny1@gmail.com>
2025-03-17 20:58:26 -0300

    Add front end pages and a partial to render the results

---
+++ TesteSygnal/ViewModels/OrderViewModel.cs
@@ -0,0 +1,24 @@
+﻿using TesteSygnal.Constants;
+using TesteSygnal.Models;
+
+namespace TesteSygnal.ViewModels;
+
+public class OrderViewModel(Order order)
+{
+    public int ControlNumber { get; set; } = order.ControlNumber;
+    public string State { get; set; } = order.State;
+
+    public string StateName =>
+        State == OrderStateConstants.Pending
+            ? "Pending"
+            : State == OrderStateConstants.InProgress
+                ? "In Progress"
+                : "Completed";
+
+    public string StateColor =>
+        State == OrderStateConstants.Pending
+            ? "secondary"
+            : State == OrderStateConstants.InProgress
+                ? "primary"
+                : "success";
+}
