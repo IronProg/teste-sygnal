6eafc29bcd96956439d16cb1256c7cb9e6934bfc
IronProg <rnberny1@gmail.com>
2025-03-18 19:54:09 -0300

    Add and implement toastr for handling user inputs

--- TesteSygnal/wwwroot/js/site.js
+++ TesteSygnal/wwwroot/js/site.js
@@ -2,6 +2,10 @@
 const $btnAddOrder = $("#btn-add-order");
 const $tableContainer = $("#table-container");
 const $loader = $("#loader");
+toastr.option = {
+    duration: 330,
+    timeOut: 2000
+};
 
 const loader = {
     show: () => $loader.css("visibility", "visible"),
@@ -17,6 +21,7 @@ $(function () {
 async function formFilterSubmitHandler(e) {
     e.preventDefault();
     try {
+        loader.show();
         const response = await $.ajax({
             url: `api/order?${$(this).serialize()}`,
             method: "GET",
@@ -26,20 +31,21 @@ async function formFilterSubmitHandler(e) {
 
         const $tableRows = $tableContainer.find("tbody tr");
 
-        console.log($tableRows);
         $tableRows.find(".btn-update-order").on("click", btnUpdateOrderClickHandler);
         $tableRows.find(".btn-delete-order").on("click", btnDeleteOrderClickHandler);
 
         return false;
     } catch (error) {
-        console.log(error);
+        toastr.error(error);
         return false;
+    } finally {
+        loader.hide();
     }
-
 }
 
 async function btnAddOrderClickHandler() {
     try {
+        loader.show();
         const response = await $.ajax({
             url: `api/order?${$(this).serialize()}`,
             method: "Post",
@@ -52,17 +58,20 @@ async function btnAddOrderClickHandler() {
         
         const $appendedRow = $tableContainer.find("tbody tr:last-of-type");
         
-        console.log($appendedRow);
         $appendedRow.find(".btn-update-order").on("click", btnUpdateOrderClickHandler);
         $appendedRow.find(".btn-delete-order").on("click", btnDeleteOrderClickHandler);
 
+        toastr.success("Order added successfully!");
     } catch (error) {
-        console.log(error);
+        toastr.error(error);
+    } finally {
+        loader.hide();
     }
 }
 
 async function btnDeleteOrderClickHandler() {
     try {
+        loader.show();
         const $tableRow = $(this).parents('tr');
         const controlNumber = $tableRow.attr('data-control-number');
     
@@ -72,12 +81,16 @@ async function btnDeleteOrderClickHandler() {
         });
         
         $tableRow.remove();
+        toastr.success("Order removed successfully!");
     } catch (error) {
-        console.log(error);
+        toastr.error(error);
+    } finally {
+        loader.hide();
     }
 }
 async function btnUpdateOrderClickHandler() {
     try {
+        loader.show();
         const $tableRow = $(this).parents('tr');
         const controlNumber = $tableRow.attr('data-control-number');
 
@@ -85,8 +98,6 @@ async function btnUpdateOrderClickHandler() {
             url: `api/order/${controlNumber}`,
             method: "PUT",
         });
-        
-        console.log(response);
         
         const $badge = $tableRow.find(".badge");
         
@@ -97,7 +108,10 @@ async function btnUpdateOrderClickHandler() {
         
         if (response.state === "completed")
             $(this).prop("disabled", true);
+        toastr.success("Order state changed to " + response.stateName);
     } catch (error) {
-        console.log(error);
+        toastr.error(error);
+    } finally {
+        loader.hide();
     }
 }
