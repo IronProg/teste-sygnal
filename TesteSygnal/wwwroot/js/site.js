bff3f6da1f1d147278464966b235cfc60c290aad
IronProg <rnberny1@gmail.com>
2025-03-17 22:34:22 -0300

    Uses form submits to filter and create orders

--- TesteSygnal/wwwroot/js/site.js
+++ TesteSygnal/wwwroot/js/site.js
@@ -1,4 +1,52 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const $formFilter = $("#form-filter");
+const $formAddOrder = $("#form-add-order");
+const $tableContainer = $("#table-container");
 
-// Write your JavaScript code.
+$(function () {
+
+    $formFilter.on("submit", formFilterSubmitHandler)
+    $formAddOrder.on("submit", formAddOrderSubmitHandler)
+})
+
+
+async function formFilterSubmitHandler(e) {
+    e.preventDefault();
+    try {
+        const response = await $.ajax({
+            url: `api/order?${$(this).serialize()}`,
+            method: "GET",
+        });
+
+        $tableContainer.html(response);
+
+        return false;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+
+}
+
+async function formAddOrderSubmitHandler(e) {
+    e.preventDefault();
+    const $controlNumberInput = $(this).find('input[name="controlNumber"]');
+    try {
+        const response = await $.ajax({
+            url: `api/order?${$(this).serialize()}`,
+            method: "Post",
+            body: JSON.stringify({
+                controlNumber: $controlNumberInput.val()
+            }),
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Content-Type","application/json");
+            }
+    })
+
+        $tableContainer.find("table tbody").append(response);
+
+        return false;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
