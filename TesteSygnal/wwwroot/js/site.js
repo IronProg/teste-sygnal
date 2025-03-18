6bdcc510a60390fc2e7e7da176cedc8e805671d8
IronProg <rnberny1@gmail.com>
2025-03-17 22:58:03 -0300

    Add jQuery logic to handle update and delete on Orders

--- TesteSygnal/wwwroot/js/site.js
+++ TesteSygnal/wwwroot/js/site.js
@@ -3,7 +3,6 @@ const $formAddOrder = $("#form-add-order");
 const $tableContainer = $("#table-container");
 
 $(function () {
-
     $formFilter.on("submit", formFilterSubmitHandler)
     $formAddOrder.on("submit", formAddOrderSubmitHandler)
 })
@@ -18,6 +17,12 @@ async function formFilterSubmitHandler(e) {
         });
 
         $tableContainer.html(response);
+
+        const $tableRows = $tableContainer.find("tbody tr");
+
+        console.log($tableRows);
+        $tableRows.find(".btn-update-order").on("click", btnUpdateOrderClickHandler);
+        $tableRows.find(".btn-delete-order").on("click", btnDeleteOrderClickHandler);
 
         return false;
     } catch (error) {
@@ -40,13 +45,60 @@ async function formAddOrderSubmitHandler(e) {
             beforeSend: function (xhr) {
                 xhr.setRequestHeader("Content-Type","application/json");
             }
-    })
+        })
 
-        $tableContainer.find("table tbody").append(response);
+        $tableContainer.find("tbody").append(response);
+        
+        const $appendedRow = $tableContainer.find("tbody tr:last-of-type");
+        
+        console.log($appendedRow);
+        $appendedRow.find(".btn-update-order").on("click", btnUpdateOrderClickHandler);
+        $appendedRow.find(".btn-delete-order").on("click", btnDeleteOrderClickHandler);
 
         return false;
     } catch (error) {
         console.log(error);
         return false;
+    }
+}
+
+async function btnDeleteOrderClickHandler() {
+    try {
+        const $tableRow = $(this).parents('tr');
+        const controlNumber = $tableRow.attr('data-control-number');
+    
+        const response = await $.ajax({
+            url: `api/order/${controlNumber}`,
+            method: "DELETE",
+        });
+        
+        $tableRow.remove();
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function btnUpdateOrderClickHandler() {
+    try {
+        const $tableRow = $(this).parents('tr');
+        const controlNumber = $tableRow.attr('data-control-number');
+
+        const response = await $.ajax({
+            url: `api/order/${controlNumber}`,
+            method: "PUT",
+        });
+        
+        console.log(response);
+        
+        const $badge = $tableRow.find(".badge");
+        
+        $badge.text(response.stateName);
+        $badge.attr("class", `badge bg-${response.stateColor}`);
+
+        $tableRow.find(".btn-delete-order").prop("disabled", true);
+        
+        if (response.state === "completed")
+            $(this).prop("disabled", true);
+    } catch (error) {
+        console.log(error);
     }
 }
