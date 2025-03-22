b98c1babc660da45b991847f10f0a725425a6e70
IronProg <rnberny1@gmail.com>
2025-03-22 15:03:13 -0300

    Improve response with react-toastify and responsiveness of pages and components

--- teste-sygnal-react/src/components/order-list/new-order/NewOrder.jsx
+++ teste-sygnal-react/src/components/order-list/new-order/NewOrder.jsx
@@ -3,7 +3,9 @@ import "./NewOrder.css"
 export default function NewOrder(props) {
     return (
         <div onClick={props.onAddOrder} className="card" id="new-order-card">
-            <p className="icon-add-order">+</p>
+            <p className="icon-add-order">
+                <img alt="plus icon" src="/plus-svgrepo-com.svg" className="w-20 h-20" />
+            </p>
         </div>
     )
 }
--- teste-sygnal-react/src/components/order-list/order-card/OrderCard.jsx
+++ teste-sygnal-react/src/components/order-list/order-card/OrderCard.jsx
@@ -3,10 +3,17 @@ import "./OrderCard.css"
 export default function OrderCard(props) {
     const isClickable = props.state !== "completed";
     const classes = isClickable ? "card order-card clickable" : "card order-card";
+
+    const stateString = props.state === "pending"
+        ? "Pending"
+        : props.state === "inprogress"
+            ? "In Progress"
+            : "Completed";
     return (
-        <div onClick={() => isClickable ? props.onUpdateOrder(props.controlNumber) : null} className={classes} data-status={props.state}>
+        <div onClick={() => isClickable ? props.onUpdateOrder(props.controlNumber) : null} className={classes}
+             data-status={props.state}>
             <p className="order-control-number">{props.controlNumber}</p>
-            <p className="order-badge">{props.state}</p>
+            <p className="order-badge">{stateString}</p>
         </div>
     )
 }
