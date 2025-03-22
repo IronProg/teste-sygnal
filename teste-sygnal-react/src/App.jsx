755825ca75a490ead30eca15255cc36b30c1370d
IronProg <rnberny1@gmail.com>
2025-03-22 00:13:56 -0300

    Initialize react project with create-react-app

---
+++ teste-sygnal-react/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './components/layout/navbar';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
