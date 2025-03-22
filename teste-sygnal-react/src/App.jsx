0d71e64e575cc99fe0cada262315a51679936a1d
IronProg <rnberny1@gmail.com>
2025-03-22 14:28:09 -0300

    Implements API Calls on React App

--- teste-sygnal-react/src/App.jsx
+++ teste-sygnal-react/src/App.jsx
@@ -3,66 +3,14 @@ import Navbar from './components/layout/Navbar';
 import FilterForm from "./components/form/FilterForm";
 import OrderList from "./components/order-list/OrderList";
 import {useEffect, useState} from "react";
-
-const orderListMockup = [
-    {
-        "controlNumber": 2,
-        "state": "completed"
-    },
-    {
-        "controlNumber": 7,
-        "state": "completed"
-    },
-    {
-        "controlNumber": 9,
-        "state": "completed"
-    },
-    {
-        "controlNumber": 13,
-        "state": "inprogress"
-    },
-    {
-        "controlNumber": 15,
-        "state": "inprogress"
-    },
-    {
-        "controlNumber": 14,
-        "state": "completed"
-    },
-    {
-        "controlNumber": 10,
-        "state": "completed"
-    },
-    {
-        "controlNumber": 24,
-        "state": "pending"
-    },
-    {
-        "controlNumber": 8,
-        "state": "completed"
-    },
-    {
-        "controlNumber": 25,
-        "state": "pending"
-    },
-    {
-        "controlNumber": 27,
-        "state": "completed"
-    },
-    {
-        "controlNumber": 20,
-        "state": "completed"
-    },
-    {
-        "controlNumber": 28,
-        "state": "pending"
-    }
-];
+import {ApiUrl} from "./globals";
+import Spinner from "./components/spinner/Spinner";
 
 function App() {
-    const [orderList, setOrderList] = useState(orderListMockup)
-    const [filteredOrderList, setFilteredOrderList] = useState(orderListMockup)
-    const [filters, setFilters] = useState({ controlNumber: "", controlNumberMax: "", state: ""})
+    const [orderList, setOrderList] = useState([])
+    const [filteredOrderList, setFilteredOrderList] = useState([])
+    const [filters, setFilters] = useState({controlNumber: "", controlNumberMax: "", state: ""})
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const newFilter = {...filters}
@@ -70,8 +18,6 @@ function App() {
     }, [orderList])
 
     useEffect(() => {
-        const urlParams = new URLSearchParams(filters);
-
         let orders = [...orderList];
         if (filters.controlNumber && filters.controlNumber !== "") {
             orders = orders.filter(item => item.controlNumber >= filters.controlNumber)
@@ -85,40 +31,99 @@ function App() {
         setFilteredOrderList(orders);
     }, [filters])
 
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                setIsLoading(true);
+                const urlParams = new URLSearchParams(filters);
+                const response = await fetch(ApiUrl + "order?" + urlParams.toString(), {
+                    method: "GET",
+                    mode: "cors"
+                });
+
+                const apiOrders = await response.json();
+
+                let orders = [...apiOrders];
+                if (filters.controlNumber && filters.controlNumber !== "") {
+                    orders = orders.filter(item => item.controlNumber >= filters.controlNumber)
+                }
+                if (filters.controlNumberMax && filters.controlNumberMax !== "") {
+                    orders = orders.filter(item => item.controlNumber <= filters.controlNumberMax)
+                }
+                if (filters.state && filters.state !== "") {
+                    orders = orders.filter(item => item.state == filters.state);
+                }
+                setOrderList(orders);
+            } catch (e) {
+                console.error(e);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchData();
+    }, [])
+
     async function addOrder() {
-        const orders = [
-            ...orderList,
-            { controlNumber: 33, state: "pending" }
-        ];
-        setOrderList(orders);
+        try {
+            const response = await fetch(ApiUrl + "order", {
+                method: "POST",
+                mode: "cors"
+            });
+
+            if (response.status !== 200) {
+                console.error("Error: ", await response.text());
+                return;
+            }
+
+            const newOrder = await response.json();
+
+            setOrderList(prevOrders => [...prevOrders, newOrder]);
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     async function updateOrder(controlNumber) {
-        const orders = [...orderList];
-        const orderIndex = orderList.findIndex(order => order.controlNumber === controlNumber);
-        const orderState = orders[orderIndex].state;
+        try {
+            const response = await fetch(ApiUrl + "order/" + controlNumber, {
+                method: "PUT",
+                mode: "cors"
+            });
 
-        if (orderState === 'pending') {
-            orders[orderIndex].state = 'inprogress';
-        } else if (orderState === 'inprogress') {
-            orders[orderIndex].state = 'completed';
+            if (response.status !== 200) {
+                console.error("Error: ", await response.text());
+                return;
+            }
+
+            const updatedOrder = await response.json();
+
+            setOrderList(prevOrders =>
+                prevOrders.map(order =>
+                    order.controlNumber === updatedOrder.controlNumber ? updatedOrder : order
+                )
+            );
+        } catch (e) {
+            console.error(e);
         }
-
-        setOrderList(orders);
     }
-
-
 
     return (
         <>
             <Navbar/>
             <div className="content-container">
-                <FilterForm onFilter={setFilters}/>
-                <hr className="my-4" />
-                <OrderList onUpdateOrder={updateOrder} onAddOrder={addOrder} orderList={filteredOrderList.toSorted((a, b) => b.controlNumber - a.controlNumber)} />
+            <FilterForm onFilter={setFilters}/>
+            <hr className="my-4"/>
+                {isLoading
+                    ? <div className="flex justify-center">
+                        <Spinner/>
+                    </div>
+                    : <OrderList onUpdateOrder={updateOrder} onAddOrder={addOrder}
+                                   orderList={filteredOrderList.toSorted((a, b) => b.controlNumber - a.controlNumber)}/>
+                }
             </div>
         </>
-    );
+    )
+        ;
 }
 
 export default App;
---
+++ teste-sygnal-react/src/globals.js
@@ -0,0 +1,1 @@
+export const ApiUrl = "https://testesygnal.fly.dev/api/rest/"
