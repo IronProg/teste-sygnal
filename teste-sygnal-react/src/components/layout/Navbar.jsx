43c897aa9b9172c9840471bf8894546381e7fabe
IronProg <rnberny1@gmail.com>
2025-03-23 00:03:37 -0300

    implements unit tests

--- teste-sygnal-react/src/components/layout/Navbar.jsx
+++ teste-sygnal-react/src/components/layout/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return <nav className="main-navbar">
     <div className="main-navbar-container">
       <span className="main-navbar-logo-container">
-          <img src="https://sygnal.group/wp-content/uploads/2024/03/logo-sygnalgroup.svg" className="h-8" alt="Flowbite Logo" />
+          <img src="https://sygnal.group/wp-content/uploads/2024/03/logo-sygnalgroup.svg" className="h-8" alt="Sygnalgroup Logo" />
       </span>
     </div>
   </nav>
